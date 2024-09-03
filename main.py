ba7c850e890cca0cc65446cc443c2b249afe48fd
kerichdev <kerichuudev@gmail.com>
2024-09-03 13:38:11 +0300

    feat: me when i ping daniele

--- main.py
+++ main.py
@@ -16,9 +16,7 @@ def main():
 
     matching = []
     data = {
-        "content": "AOPS 15 IS OUT @dgigantino @dgigantino @dgigantino @dgigantino @dgigantino @dgigantino @dgigantino @dgigantino @dgigantino @dgigantino @dgigantino @dgigantino @dgigantino @dgigantino @dgigantino @dgigantino",
-        "embeds": "null",
-        "attachments": []
+        "content": "AOSP 15 IS OUT <@928393531033264129> <@928393531033264129> <@928393531033264129> <@928393531033264129> <@928393531033264129> <@928393531033264129> <@928393531033264129> <@928393531033264129> <@928393531033264129> <@928393531033264129> <@928393531033264129> <@928393531033264129> <@928393531033264129> <@928393531033264129> <@928393531033264129> <@928393531033264129>"
     }
 
     while len(matching) == 0:
@@ -31,13 +29,18 @@ def main():
             tag_list.append(tag)
         matching = [s for s in tag_list if 'android-15' in s or 'android15' in s]
         if len(matching) > 0:
-            result = requests.post(webhookUrl, json = data)
-            try:
-                result.raise_for_status()
-            except requests.exceptions.HTTPError as err:
-                print(err)
+            if 'android-15-beta-2-gpl' or 'android-15-dp-2-gpl' or 'android-15-qpr1-beta-1-gpl' or 'android-15-dp-2-gpl' or 'android-15-beta-2-gpl' or 'android-15-qpr1-beta-1-gpl' in matching:
+                print('wrong androd 15 :(')
+                matching = []
+                time.sleep(60)
             else:
-                print(f"Payload delivered successfully, code {result.status_code}.")
+                result = requests.post(webhookUrl, json = data)
+                try:
+                    result.raise_for_status()
+                except requests.exceptions.HTTPError as err:
+                    print(err)
+                else:
+                    print(f"Payload delivered successfully, code {result.status_code}.")
         else:
             print('Still no Androd 15 :(')
             time.sleep(60)
